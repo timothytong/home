18f3350de1a132b52c7224f8dc8ba968d134cd59
timothytong <timothy.tong@hotmail.ca>
2014-10-30 21:29:01 -0400

    Separated JS files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,222 @@
+$(document).ready(function () {
+            var firstRun = true;
+            $(window).resize(function () {
+                resizeContent(firstRun);
+            });
+            function checkWidth() {
+                width = $(window).width();
+            }
+            $('#menu-icon').click(function (e) {
+                e.preventDefault();
+                checkWidth();
+                if (width <= 959) {
+                    $('#sections').toggleClass("sections-open");
+                }
+            });
+            $('#intro #scrl-dwn-btn').click(function () {
+                $("html, body").animate({scrollTop: $('#about').offset().top - $('.navigation').height()}, 1000, 'swing');
+            });
+            $('#sections ul li').click(function (e) {
+                e.preventDefault();
+                var body = $("html, body");
+                if ($(this).is(".about")) {
+                    body.animate({scrollTop: $('#about').offset().top - $('.navigation').height()}, 1000, 'swing');
+                }
+                else if ($(this).is(".work")) {
+                    body.animate({scrollTop: $('#work').offset().top - $('.navigation').height()}, 1000, 'swing');
+                }
+                else if ($(this).is(".projects")) {
+                    body.animate({scrollTop: $('#projects').offset().top - $('.navigation').height()}, 1000, 'swing');
+                }
+                else if ($(this).is(".contact")) {
+                    body.animate({scrollTop: $('#contact').offset().top - $('.navigation').height()}, 1000, 'swing');
+                }
+            });
+            $('#initial-and-name .back-to-top').click(function (e) {
+                e.preventDefault();
+                var body = $("html, body");
+                body.animate({scrollTop: 0}, 'slow', 'swing', function () {
+                });
+            });
 
+            var browser = window.navigator.userAgent;
+            var msie = browser.indexOf("MSIE ");
+            var safari = browser.indexOf("Safari");
+            var chrome = browser.indexOf("Chrome");
+            var firefox = browser.indexOf("Firefox");
+            if (msie > 0) {
+                if (parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))) <= 8) {
+                    alert("Friendly advice: Please consider upgrading your browser.")
+                }//IE VERSION
+                $('svg').hide();
+            }
+            else if (firefox > 0) {
+                $(".work span").addClass("firefox-work");
+                $(".projects, .contact").hover(function () {
+                    $(this).find("span").toggleClass("firefox-projects-contact-hover");
+                });
+                $(".about, .work").hover(function () {
+                    $(this).find("span").toggleClass("firefox-about-work-hover");
+                });
+            }
+            else if (safari > 0 || chrome > 0) {
+                $(".about span, .work span").addClass("safari-chrome-about-work");
+                $(".about, .work").hover(function () {
+                    $(this).find("span").toggleClass("safari-chrome-about-work-hover");
+                });
+                $(".projects span").addClass("safari-chrome-projects");
+                $(".contact span, .work span").addClass("safari-chrome-contact");
+                $(".projects, .contact").hover(function () {
+                    $(this).find("span").toggleClass("safari-chrome-projects-contact-hover");
+                });
+            }
+            resizeContent(firstRun);
+            firstRun = false;
+            var navHidden = true;
+            var navbarHeight = $('.navigation').outerHeight();
+            var menuHighlight = 0;
+
+            $(window).scroll(function (event) {
+                var width = $(window).width();
+                var curOffset = $(this).scrollTop();
+                if (curOffset >= $("#about").offset().top - navbarHeight && navHidden) {
+                    $(".navigation").slideDown();
+                    navHidden = false;
+                }
+                if (curOffset < $("#about").offset().top - navbarHeight && !navHidden) {
+                    $(".navigation").slideUp();
+                    navHidden = true;
+                }
+                $menuSections = $(".navigation #sections ul");
+                if (curOffset < $("#about").offset().top - navbarHeight && menuHighlight !== 0) {
+                    resetMenuHighlight();
+                    menuHighlight = 0;
+                }
+                else if (curOffset >= $("#about").offset().top - navbarHeight && curOffset < $("#work").offset().top - navbarHeight && menuHighlight !== 1) {
+                    resetMenuHighlight();
+                    menuHighlight = 1;
+                    if (width < 960) {
+                        $(".about").addClass("menu-highlight").find("a").addClass("menu-text-highlight-mobile");
+                    }
+                    else {
+                        $(".about").find("a").addClass("menu-text-highlight");
+                    }
+
+                }
+                else if (curOffset >= $("#work").offset().top - navbarHeight && curOffset < $("#projects").offset().top - navbarHeight && menuHighlight !== 2) {
+                    resetMenuHighlight();
+                    $(".navigation").css("background-color", "rgba(39,186,189,1)");
+                    $(".name").css("color", "white");
+                    $("#menu-icon").css("color","white");
+                    menuHighlight = 2;
+                    if (width < 960) {
+                        $(".work").addClass("menu-highlight").find("a").addClass("menu-text-highlight-mobile");
+                    }
+                    else {
+                        $(".work").find("a").addClass("menu-text-highlight");
+                        $("#sections ul li span a").css("color", "white");
+                    }
+                }
+                else if (curOffset >= $("#projects").offset().top - navbarHeight && curOffset < $("#contact").offset().top - navbarHeight && menuHighlight !== 3) {
+                    resetMenuHighlight();
+                    menuHighlight = 3;
+                    if (width < 960) {
+                        $(".projects").addClass("menu-highlight").find("a").addClass("menu-text-highlight-mobile");
+                    }
+                    else {
+                        $(".projects").find("a").addClass("menu-text-highlight");
+                    }
+                }
+                else if (curOffset >= $("#contact").offset().top - navbarHeight && menuHighlight !== 4) {
+                    resetMenuHighlight();
+                    menuHighlight = 4;
+                    if (width < 960) {
+                        $(".contact").addClass("menu-highlight").find("a").addClass("menu-text-highlight-mobile");
+                    }
+                    else {
+                        $(".contact").find("a").addClass("menu-text-highlight-mobile");
+                    }
+                }
+            });
+            $('#menu-icon').bind('touchstart touchend', function () {
+                $(this).toggleClass('menu-icon-highlight-mobile');
+            });
+            $("#sections ul li a").bind('touchstart touchend', function () {
+                $(this).toggleClass('menu-items-highlight-mobile');
+            });
+            $("#initial-and-name .back-to-top").bind('touchstart touchend', function () {
+                $(this).toggleClass('back-to-top-highlight-mobile');
+            });
+            $("#vello-link").bind('touchstart touchend', function () {
+                $(this).toggleClass('vello-link-highlight');
+            });
+        });
+        function resetMenuHighlight() {
+            $menuSections = $(".navigation #sections ul");
+            $menuSections.find('li').each(function () {
+                $(this).removeClass("menu-highlight");
+                $(this).find("a").removeClass("menu-text-highlight-mobile");
+                $(this).find("a").removeClass("menu-text-highlight");
+            });
+            $(".navigation").css("background-color", "rgba(64,64,64,0.85)");
+            $(".name").css("color", "#ccc");
+            $("#sections ul li span a").css("color", "#ccc");
+            $("#menu-icon").css("color","#ccc");
+        }
+        //resizes height of Intro div.
+        function resizeContent(firstRun) {
+            var browser = window.navigator.userAgent;
+//            var msie = browser.indexOf("MSIE ");
+//            var safari = browser.indexOf("Safari");
+//            var chrome = browser.indexOf("Chrome");
+            var firefox = browser.indexOf("Firefox");
+            $height = $(window).height();
+            $("#intro").height($height);
+            $width = $(window).width();
+            $introContainer = $("#intro .container");
+            $introScrlBtn = $("#intro #scrl-dwn-btn");
+            $jobDetPanel = $(".job-details");
+            $abtSec = $("#about");
+            $workSec = $("#work");
+            $abtFilter = $("#about-filter");
+            $velloLogo = $('#work .job-details img');
+            if ($width <= 1320) {
+                var abtContHeight = $("#about .container").height();
+                if (!firstRun) {
+                    abtContHeight += 20;
+                }
+                else {
+                    abtContHeight -= 10;
+                    if (firefox > 0) {
+                        abtContHeight += 30;
+                    }
+                }
+                $abtSec.css("height", abtContHeight + "px");
+                $abtFilter.css("height", abtContHeight + "px");
+            } else {
+                $abtSec.css("height", "439px");
+                $abtFilter.css("height", "439px");
+            }
+
+            //position of #intro .container
+            $introContainer.css("position", "absolute").css("left", (($width / 2) - ($introContainer.width() / 2)) + "px").css("top", (($height / 2) - ($introContainer.height() / 2) - 20) + "px");
+            $introScrlBtn.css("left", (($width / 2) - ($introScrlBtn.width() / 2)) + "px");
+            $jobDetPanel.css("left", (($width / 2) - ($jobDetPanel.width() / 2)) + "px");
+            $workSec.css("height", $jobDetPanel.height() + 20 + "px");
+
+            if ($width <= 500) {
+                $introContainer.find("p").html("Technology Enthusiast</br>Perfectionist</br>Animal Lover");
+                $introContainer.find("h1").html("timothy</br>tong");
+                $("#about .section-header").html("A<span>bout.</span>");
+                $("#work .section-header").html("W<span>ork.</span>");
+            } else {
+                $introContainer.find("p").html("Technology Enthusiast <span class='dot'><i class='fa fa-circle'></i></span> Perfectionist <span class='dot'><i class='fa fa-circle'></i></span> Animal Lover");
+                $introContainer.find("h1").html("timothy tong");
+                $("#about .section-header").html("A<span>bout me.</span>");
+                $("#work .section-header").html("W<span>ork experience.</span>");
+                $("#intro-name").addClass("intro-name-desktop");
+                if ($height <= 400) {
+                    $introContainer.find("h1").css("margin-top", "20px");
+                }
+            }
+        }
