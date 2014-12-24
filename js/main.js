558d975f6b4f00ef9e657f3a32a8751d894ef331
timothytong <timothy.tong@hotmail.ca>
2014-12-24 18:15:37 -0500

    Projects hover animation complete

--- js/main.js
+++ js/main.js
@@ -22,9 +22,11 @@ var $menuContact = $(".contact");
 var $menuAbout = $(".about");
 var $menuWork = $(".work");
 var $menuProj = $(".projects");
-var $abtSecHeader = $("#about .section-header");
+var $abtSecHeader = $(".section-header");
 var $wrkSecHeader = $("#work .section-header");
 var $projSecHeader = $("#projects .section-header");
+var $projHoverArea = $(".hover-area");
+var $projDes = $(".proj-des");
 function resetMenuHighlight() {
     var menuSections = $(".navigation #sections ul");
     $("#vello-link").css("color", "black");
@@ -38,7 +40,7 @@ function resetMenuHighlight() {
 
 }
 //responsiveness
-var resizeContent = function () {    
+var resizeContent = function () {
     var height = $(window).height();
     $introSection.height(height);
     var width = $(window).width();
@@ -48,25 +50,25 @@ var resizeContent = function () {
         $aboutSection.css("height", abtContHeight + "px");
         $aboutFilter.css("height", abtContHeight + "px");
     }
-    if (width >= 1491){
-        $abtCont.css("margin-left", ((width / 2) - ($abtCont.width()/2)) + "px");
+    if (width >= 1491) {
+        $abtCont.css("margin-left", ((width / 2) - ($abtCont.width() / 2)) + "px");
     }
-    else{
+    else {
         $abtCont.css("margin-left", "auto");
     }
-    
+
     //position of #intro .container
     $introCont.css("position", "absolute").css("left", ((width / 2) - ($introCont.width() / 2)) + "px").css("top", ((height / 2) - ($introCont.height() / 2) - 20) + "px");
-    $abtSecHeader.css("margin-left", ((width / 2) - ($abtSecHeader.width()/2)) + "px");
-    $wrkSecHeader.css("margin-left", ((width / 2) - ($wrkSecHeader.width()/2)) + "px");
-    $projSecHeader.css("margin-left", ((width / 2) - ($projSecHeader.width()/2)) + "px");
+    $abtSecHeader.css("margin-left", ((width / 2) - ($abtSecHeader.width() / 2)) + "px");
+    $wrkSecHeader.css("margin-left", ((width / 2) - ($wrkSecHeader.width() / 2)) + "px");
+    $projSecHeader.css("margin-left", ((width / 2) - ($projSecHeader.width() / 2)) + "px");
     $introScrlBtn.css("left", ((width / 2) - ($introScrlBtn.width() / 2)) + "px");
     $jobDets.css("left", ((width / 2) - ($jobDets.width() / 2)) + "px");
     $workSection.css("height", $jobDets.height() + $abtSecHeader.height() + 40 + "px");
-    $units.css("height", $units.width()+"px");
-    
+    $units.css("height", $units.width() + "px");
+
 //    $projectSec.css("height", $projList.height() + 120 + "px");
-    $projectSec.css("height", $projCont.height() + $projSecHeader.height() + 70 + 'px'); 
+    $projectSec.css("height", $projCont.height() + $projSecHeader.height() + 70 + 'px');
 //    $projList.css("margin-top", $("#projects .container .section-header").height() + 30 + "px");
 
     if (width <= 500) {
@@ -90,12 +92,17 @@ var resizeContent = function () {
     }
     $velloLogo.css("top", $pivotalLine.offset().top - $workSection.offset().top - 70 + "px");
 
+    $projHoverArea.css("width", $projHoverArea.parent().width() + "px").css("margin-top", $projHoverArea.parent().height() * 0.75 + "px");
+    $projDes.css("width", $projDes.parent().width() + "px").css("margin-top", $projDes.parent().height() * 0.5 + "px");
     //Timeline
 //    $('.circle-pivotal').css("top", $('.pivotal-labs .company-name').offset().top - $workSection.offset().top + "px").css('left', $('.pivotal-labs .position').offset().left / 2 + 'px');
 //    $('.circle-vello').css("top", $('.vello .company-name').offset().top - $workSection.offset().top + "px").css('left', $('.vello .position').offset().left / 2 + 'px');
 //    $('#timeline').css("height", $('.pivotal-labs').height() + $('.vello').height() - 50 + 'px').css("top", $('.pivotal-labs .company-name').offset().top + $(".circle").height() / 2 - $workSection.offset().top + "px").css('left', $('.circle').offset().left + 8 + "px");
-};
+    $(".hover-area .line-separator").each(function () {
+        $(this).css("left", "15%");
+    });
 
+};
 
 $(document).ready(function () {
     var width = $(window).width();
@@ -164,7 +171,7 @@ $(document).ready(function () {
     var safari = browser.indexOf("Safari");
     var chrome = browser.indexOf("Chrome");
     var firefox = browser.indexOf("Firefox");
-    
+
     if (msie > 0) {
         if (parseInt(browser.substring(msie + 5, browser.indexOf(".", msie))) <= 8) {
             alert("Friendly advice: Please consider upgrading your browser.");
@@ -191,9 +198,9 @@ $(document).ready(function () {
             $(this).find("span").toggleClass("safari-chrome-projects-contact-hover");
         });
     }
-    if(chrome > 0 || parseInt(browser.substring(msie + 5, browser.indexOf(".", msie))) <= 9){
+    if (chrome > 0 || parseInt(browser.substring(msie + 5, browser.indexOf(".", msie))) <= 9) {
         $introSection.addClass("intro-static");
-    }else{
+    } else {
         $introSection.parallax({imageSrc: './images/apple-bg.jpg'});
     }
     $('#pivotal-link').hover(function () {
@@ -211,27 +218,11 @@ $(document).ready(function () {
         var curOffset = $(this).scrollTop();
         if (curOffset >= $aboutSection.offset().top - 3 * navbarHeight && navHidden) {
             navHidden = false;
-//            if(chrome>0){
-//                console.log("this is chrome");
-//                $navigation.slideDown();
-//            }
-//            else{
-//                console.log("this is not chrome");
-                $navigation.css("margin-top","0px");
-//            }
-            
+            $navigation.css("margin-top", "0px");
         }
-        if (curOffset < $aboutSection.offset().top / 2 && !navHidden) { 
+        if (curOffset < $aboutSection.offset().top / 2 && !navHidden) {
             navHidden = true;
-//            if(chrome > 0){
-//                console.log("this is chrome");
-//                $navigation.slideUp();
-//            }else{
-//                console.log("this is not chrome");
-                $navigation.css("margin-top", -navbarHeight+"px");
-//            }
-            
-            
+            $navigation.css("margin-top", -navbarHeight + "px");
         }
         if (curOffset < $aboutSection.offset().top - 3 * navbarHeight) {
             resetMenuHighlight();
@@ -281,13 +272,19 @@ $(document).ready(function () {
         else if (curOffset >= $quoteSec.offset().top - navbarHeight - 30 && curOffset < $projectSec.offset().top - navbarHeight && menuHighlight !== 0) {
             menuHighlight = 0;
             resetMenuHighlight();
-            $navigation.css("background-color", "rgba(64,64,64,1)");
             //$(".circle-pivotal").removeClass("circle-pivotal-color");
             //$(".circle-vello").removeClass("circle-vello-color");
+            if (width < 960) {
+                $menuProj.addClass("menu-highlight").find("a").addClass("menu-text-highlight-mobile");
+            }
+            else {
+                $menuProj.find("a").addClass("menu-text-highlight");
+            }
         }
         else if (curOffset >= $projectSec.offset().top - navbarHeight && curOffset < $contactSec.offset().top - navbarHeight && menuHighlight !== 4) {
             menuHighlight = 4;
             resetMenuHighlight();
+            $navigation.css("background-color", "rgba(64,64,64,1)");
             //$(".circle-pivotal").removeClass("circle-pivotal-color");
             //$(".circle-vello").removeClass("circle-vello-color");
             if (width < 960) {
@@ -320,4 +317,34 @@ $(document).ready(function () {
         $(this).toggleClass('back-to-top-highlight-mobile');
     });
     setTimeout(resizeContent, 300);
+
+    $(".hover-area .line-separator").each(function () {
+        $(this).css("left", "15%");
+        $(this).css("margin-top", "0px");
+    });
+
+    $(".projUnit").hover(function () {
+        var hoverArea = $(this).find(".hover-area");
+        var description = hoverArea.find(".proj-des");
+        hoverArea.animate({
+            "margin-top": $(this).height() - hoverArea.height() + "px",
+        }, {duration: 300, easing: "swing", queue: false});
+        hoverArea.fadeIn(150);
+        
+        description.delay(100).animate({
+            "margin-top": -5 + "px",
+        }, {duration: 300, easing: "swing", queue: false});
+        description.fadeIn(150);
+    }, function () {
+        var hoverArea = $(this).find(".hover-area");
+        var description = hoverArea.find(".proj-des");
+        description.animate({
+            "margin-top": hoverArea.height() * 0.5 + "px"
+        }, {duration: 300, easing: "swing", queue: false});
+        description.fadeOut(150);
+        hoverArea.delay(100).animate({
+            "margin-top": $(this).height() * 0.75 + "px"
+        },{duration: 300, easing: "swing", queue: false});
+        hoverArea.fadeOut(150);
+    });
 });
