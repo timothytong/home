9af78a17ad5a7978b27a4d01a41260c1412a3714
timothytong <timothy.tong@hotmail.ca>
2015-06-10 12:56:34 -0400

    Update

--- contact_form.php
+++ contact_form.php
@@ -8,25 +8,25 @@ $email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing E-mail.
 // After sanitization Validation is performed
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
-	$subject = 'timothytong.com - New Message From '.$name;
-	// To send HTML mail, the Content-type header must be set.
-	$headers = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-	$headers .= 'From:' . $email. "\r\n"; // Sender's Email
-	$headers .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
-	$template = '<div style="padding:50px; color:white;">'
-	. 'Name: ' . $name . '<br/>'
-	. 'Email: ' . $email . '<br/>'
-	. 'Message:<br/>' . $message . '<br/><br/>'
-	. 'Your message has been sent to Timothy Tong.'
-	. '<br/>'
-	. 'I\'ll get back to you as soon as possible - Tim</div>';
-	$sendmessage = "<div style=\"background-color:#1F1F1F; color:white;\">" . $template . "</div>";
-	// Message lines should not exceed 70 characters (PHP rule), so wrap it.
-	$sendmessage = wordwrap($sendmessage, 70);
-	// Send mail by PHP Mail Function.
-	mail("timothykytong@gmail.com", $subject, $sendmessage, $headers);
-	echo "Got it. I'll get back to you ASAP.";
+$subject = 'timothytong.com - New Message From '.$name;
+// To send HTML mail, the Content-type header must be set.
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From:' . $email. "\r\n"; // Sender's Email
+$headers .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
+$template = '<div style="padding:50px; color:white;">'
+. 'Name: ' . $name . '<br/>'
+. 'Email: ' . $email . '<br/>'
+. 'Message:<br/>' . $message . '<br/><br/>'
+. 'Your message has been sent to Timothy Tong.'
+. '<br/>'
+. 'I\'ll get back to you as soon as possible - Tim</div>';
+$sendmessage = "<div style=\"background-color:#1F1F1F; color:white;\">" . $template . "</div>";
+// Message lines should not exceed 70 characters (PHP rule), so wrap it.
+$sendmessage = wordwrap($sendmessage, 70);
+// Send mail by PHP Mail Function.
+mail("timothykytong@gmail.com", $subject, $sendmessage, $headers);
+echo "Got it. I'll get back to you ASAP.";
 
 } else {
 echo "<span>* invalid email *</span>";
--- js/main.js
+++ js/main.js
@@ -26,7 +26,6 @@ var $abtSecHeader = $(".section-header");
 var $wrkSecHeader = $("#work .section-header");
 var $projSecHeader = $("#projects .section-header");
 var $contactSecHead = $("#contact .section-header");
-var $contactCont = $("#contact .container");
 var $projHoverArea = $(".hover-area");
 var $projDes = $(".proj-des");
 var $projImg = $(".proj-img");
@@ -75,7 +74,6 @@ var resizeContent = function () {
 //    $projectSec.css("height", $projList.height() + 120 + "px");
     $projectSec.css("height", $projCont.height() + $projSecHeader.height() + 70 + 'px');
 //    $projList.css("margin-top", $("#projects .container .section-header").height() + 30 + "px");
-$contactSec.css("height", $contactCont.height() + $contactSecHead.height() + 70 + 'px');
 
     if (width <= 500) {
         $introCont.find("p").html("Technology Enthusiast</br>Perfectionist</br>Animal Lover");
@@ -94,16 +92,12 @@ $contactSec.css("height", $contactCont.height() + $contactSecHead.height() + 70 
         $introName.addClass("intro-name-desktop");
         
     }
-    if (width < 660) {
+    if (width < 700) {
             $(".note, #messageArea").css("width",width - 150 + "px");
             $(".textInput").css("width",width - 200 + "px");
              
-            $("table").css("width", width - 50 + "px");
-            if(width < 410){
-                $("table #name, table #email").css("width", $("#messageArea").width() + 10+"px");
-            }
+            $("table").css("width", width - 70 + "px");
         }
-    
     $velloLogo.css("top", $pivotalLine.offset().top - $workSection.offset().top - 70 + "px");
 
     $projHoverArea.css("width", $projHoverArea.parent().width() + "px").css("margin-top", $projHoverArea.parent().height() * 0.75 + "px");
